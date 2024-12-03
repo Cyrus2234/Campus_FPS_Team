57768d954c649268794992cea524388ee306e359
KinkaydeM <klamcraft11@gmail.com>
2024-12-03 16:10:57 -0500

    made button script
    created a button script with button functions

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void Resume()
+    {
+
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void LoadScene(string _scene)
+    {
+        SceneManager.LoadScene(_scene);
+    }
+}
