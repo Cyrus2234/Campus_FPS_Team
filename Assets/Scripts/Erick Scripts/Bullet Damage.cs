80398b787b0a6bbc333705a103cee6932624d8ca
ejjimenezsalto <162148122+ejjimenezsalto@users.noreply.github.com>
2024-12-05 14:36:28 -0500

    Bullet script
    Completed on creating the bullet script

---
+++ Assets/Scripts/Erick Scripts/Bullet Damage.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    enum damageType {moving, stationary}
+    [SerializeField] damageType type;
+    [SerializeField] Rigidbody rb;
+
+    //Bullet stats
+    [SerializeField] int damageAmount;
+    [SerializeField] int bulletSpeed;
+    [SerializeField] int destoryTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (type == damageType.moving)
+        {
+            rb.velocity = transform.forward * bulletSpeed;
+            Destroy(gameObject, destoryTime);
+        }
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+            return;
+
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if(dmg != null)
+            dmg.takeDamage(damageAmount);
+
+        if (type == damageType.moving)
+            Destroy(gameObject);
+    }
+}
