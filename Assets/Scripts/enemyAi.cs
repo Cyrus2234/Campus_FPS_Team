592a7f14405d2e943a61aa6ca54b45c80fea5afc
getSkai <agamer.sup@gmail.com>
2024-12-05 13:45:41 -0500

    enemy ai script added

---
+++ Assets/Scripts/enemyAi.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAi : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [SerializeField] Transform shootPos;
+    [SerializeField] Transform headPos;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+    [SerializeField] int fov;
+
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+    Vector3 playerDir;
+
+    bool playerInRange;
+    bool isShooting;
+
+    Color colorOrig;
+
+    float angleToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorOrig = model.material.color;
+        Gamemanager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   if (playerInRange && canSeePlayer())
+        {
+
+        }
+    }
+
+    bool canSeePlayer()
+    {
+
+        playerDir = Gamemanager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+
+        Debug.DrawRay(headPos.position, playerDir);
+        RaycastHit hit;
+
+        if(Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= fov)
+            {
+
+                agent.SetDestination(Gamemanager.instance.player.transform.position);
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    faceTarget();
+                }
+
+                if (!isShooting)
+                {
+                    StartCoroutine(shoot());
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation( new Vector3 (playerDir.x, 0, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot , Time.deltaTime * faceTargetSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+
+
+    public void takeDamage(int amount)
+    {
+       HP -= amount;
+        StartCoroutine(flashRed());
+        if (HP <= 0)
+        {
+            // I'm Dead
+            Gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+
+
+}
