ad7a8bdfdf6a5180b4a4d90c8b1f6e59fa8f9a0e
ejjimenezsalto <162148122+ejjimenezsalto@users.noreply.github.com>
2024-12-17 15:22:09 -0500

    getting ready  push
    I am getting ready to push the gun stats script

--- Assets/Scripts/GunStats.cs
+++ Assets/Scripts/GunStats.cs
@@ -9,6 +9,7 @@ public class GunStats : ScriptableObject
     public GameObject Model;
     public int GunDamage;
     public float FireRate;
+    public int bulletDist;
 
     public ParticleSystem hitEffect;
     public AudioClip[] GunSound;
