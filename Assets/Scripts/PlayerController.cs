954fe3aa3b83d17c4c50b83e0143b7442a310777
Devon Ramroop <dramroop@student.fullsail.edu>
2024-12-18 18:14:19 -0500

    Created loadout menu (L) and added gun color change functionality and throwable object change functionality. WIP: shot type change functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -273,6 +273,11 @@ public class PlayerController : MonoBehaviour, IDamage, IStunnable
         GameManager.instance.playerHPBar.fillAmount = (float)health / healthOriginal;
     }
 
+    public void changeThrowable(GameObject throwable)
+    {
+        throwableObject = throwable;
+    }
+
     /*
     public void getGunStats(gunStats gun)
     {
